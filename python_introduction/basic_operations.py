1cf60d8464f40f54d74a955914fbd205a59a6c5b
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-13 21:44:01 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Basic Arithmetic Exercise
+number1 = 10
+number2 = 5
+
+# Addition
+sum = number1 + number2
+print(f"Addition of {number1} and {number2} is: {sum}")
+
+# Subtraction
+difference = number1 - number2
+print(f"Subtraction of {number1} and {number2} is: {difference}")
+
+# Multiplication
+multiplication = number1 * number2
+print(f"Multiplication of {number1} and {number2} is: {multiplication}")
